e2be9c601151464612396629e6b3c101b2105fba
Facundo Martinez <facu@spect.ro>
2017-04-24 16:44:45 -0300

    master: 	* Fixing docker ci tests

--- docker-ci-test-resources/assert_docker_ci.py
+++ docker-ci-test-resources/assert_docker_ci.py
@@ -3,7 +3,7 @@ import os
 
 reports_volume_data_path = '/var/lib/docker/volumes/ReportsVolume/_data'
 
-with open('test-tix-report.json', 'r') as expected_report_file:
+with open('docker-ci-test-resources/test-tix-report.json', 'r') as expected_report_file:
     expected_tix_report = json.load(expected_report_file)
 
 users_dirs = os.listdir(reports_volume_data_path)
