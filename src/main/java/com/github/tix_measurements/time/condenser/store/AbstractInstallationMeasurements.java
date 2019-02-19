8dc387ccd67ca42bfa3655bf251ba704d1d9cafa
Agustin Carames <aecarames@gmail.com>
2019-02-19 11:27:58 -0300

    Fix algorithm used to build measure set

--- src/main/java/com/github/tix_measurements/time/condenser/store/AbstractInstallationMeasurements.java
+++ src/main/java/com/github/tix_measurements/time/condenser/store/AbstractInstallationMeasurements.java
@@ -1,6 +1,9 @@
 package com.github.tix_measurements.time.condenser.store;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,102 +27,95 @@ public abstract class AbstractInstallationMeasurements implements InstallationMe
 		int lastReportsCount;
 		do {
 			lastReportsCount = sampleStartTimes().size();
-			logger.debug("Installation {} check and extract starts with {}", getInstallationId(), sampleStartTimes().size());
+			logger.info("Installation {} check and extract starts with {}", getInstallationId(), sampleStartTimes().size());
 			Optional<SubmittablePacketSet> extracted = checkAndExtractOnce();
 			if (extracted.isPresent()) {
 				return extracted;
 			}
 		} while (lastReportsCount != sampleStartTimes().size());
-		logger.debug("Installation {} check and extract ends with {}", getInstallationId(), sampleStartTimes().size());
+		logger.info("Installation {} check and extract ends with {}", getInstallationId(), sampleStartTimes().size());
 		return Optional.empty();
 	}
 	
+	static private int observationsIn(TixDataPacket packet) {
+		return packet.getMessage().length / 45;
+	}
+	
+	private Optional<Integer> find_gap_cut(List<TixDataPacket> packet) {
+		long diff = MAX_ACCEPTED_REPORT_GAP * 1000000000L;
+		Optional<Integer> gap_cut = Optional.empty();
+		for (int i = 0; i + 1 < packet.size(); ++i) {
+			final TixDataPacket prev = packet.get(i);
+			final TixDataPacket next = packet.get(i + 1);
+			final long next_diff = next.getInitialTimestamp() - prev.getInitialTimestamp(); 
+			if (next_diff > diff) {
+				diff = next_diff;
+				gap_cut = Optional.of(i);
+			}
+		}
+		return gap_cut;
+	}
+	
 	protected Optional<SubmittablePacketSet> checkAndExtractOnce() throws IOException {
-		final int initialNumberOfPackets = Math.min(sampleStartTimes().size(), MAX_REPORTS_USED_AT_ONCE / MAX_MEASURES_PER_PACKET);
-
-		List<Long> availableStartTimes = sampleStartTimes().subList(0, Math.max(0, initialNumberOfPackets));
-		for (int i = 0; i + 1 < availableStartTimes.size(); i++) {
-			if (availableStartTimes.get(i) + MAX_ACCEPTED_REPORT_GAP >= availableStartTimes.get(i + 1)) {
+		List<TixDataPacket> processable_reports = new ArrayList<>();
+		List<Long> remaining_start_times = new ArrayList<>(sampleStartTimes());
+		
+		while (processable_reports.stream().mapToInt(AbstractInstallationMeasurements::observationsIn).sum() < MIN_REQUIRED_REPORTS && !remaining_start_times.isEmpty()) {
+			final long new_start = remaining_start_times.remove(0);
+			final TixDataPacket new_report = this.get(Arrays.asList(new_start)).get(0);
+			
+			if (processable_reports.size() > 0) {
+				final InetAddress processable_reports_ip = processable_reports.get(0).getFrom().getAddress();
+				final InetAddress new_report_ip = new_report.getFrom().getAddress();
+				if (!processable_reports_ip.equals(new_report_ip)) {
+					logger.info("Installation {} dropping {} measures with wrong ip", getInstallationId(), processable_reports.size() + 1);
+					this.delete(sampleStartTimes().subList(0, processable_reports.size() + 1));
+					return Optional.empty();
+				}
+			}
+			processable_reports.add(new_report);
+			
+			if (processable_reports.stream().mapToInt(AbstractInstallationMeasurements::observationsIn).sum() <= MIN_REQUIRED_REPORTS) {
 				continue;
 			}
 			
-			if ((i + 1) * MAX_MEASURES_PER_PACKET < MIN_REQUIRED_REPORTS) {
-				// This set is too small and can't grow
-				logger.info("Installation {} dropping {} start time measures", getInstallationId(), i + 1);
-				delete(availableStartTimes.subList(0, i + 1));
-				return Optional.empty();
+			final Optional<Integer> gap_cut = find_gap_cut(processable_reports);
+			final List<TixDataPacket> toSubmit;
+			final List<Long> toDelete;
+			if (gap_cut.isPresent()) {
+				if (processable_reports.stream().limit(gap_cut.get()).mapToInt(AbstractInstallationMeasurements::observationsIn).sum() < MIN_REQUIRED_REPORTS) {
+					logger.info("Installation {} dropping {} measures with too much separation", getInstallationId(), processable_reports.size() + 1);
+					this.delete(sampleStartTimes().subList(0, gap_cut.get() + 1));
+					return Optional.empty();
+				} else {
+					toSubmit = processable_reports.subList(0, gap_cut.get() + 1);
+					toDelete = sampleStartTimes().subList(0, toSubmit.size() / 2);
+				}
 			} else {
-				// This set might be large enough but can't grow
-				logger.info("Installation {} restricted to {} start time measures", getInstallationId(), i + 1);
-				availableStartTimes = availableStartTimes.subList(0, i + 1);
-				break;
-			}
-		}
-		if (availableStartTimes.size() * MAX_MEASURES_PER_PACKET < MIN_REQUIRED_REPORTS) {
-			// This set is too small and may grow later
-			logger.info("Installation {} delayed with {} start time measures", getInstallationId(), availableStartTimes.size());
-			return Optional.empty();
-		}
-
-		
-		List<TixDataPacket> packetsAvailable = get(availableStartTimes);
-		for (int i = 0; i + 1 < packetsAvailable.size(); i++) {
-			if (packetsAvailable.get(i).getFrom().equals(packetsAvailable.get(0).getFrom())) {
-				continue;
-			}
-			
-			if ((i + 1) * MAX_MEASURES_PER_PACKET < MIN_REQUIRED_REPORTS) {
-				// This set is too small and can't grow
-				logger.info("Installation {} dropping {} packets", getInstallationId(), i + 1);
-				delete(availableStartTimes.subList(0, i + 1));
-				return Optional.empty();
-			} else {
-				// This set might be large enough but can't grow
-				logger.info("Installation {} restricted to {} packets", getInstallationId(), i + 1);
-				availableStartTimes = availableStartTimes.subList(0, i + 1);
-				packetsAvailable = packetsAvailable.subList(0, i + 1);
-				break;
-			}
-		}
-		if (packetsAvailable.size() * MAX_MEASURES_PER_PACKET < MIN_REQUIRED_REPORTS) {
-			// This set is too small and may grow later
-			logger.info("Installation {} delayed with {} packets", getInstallationId(), availableStartTimes.size());
-			return Optional.empty();
-		}
-
-		List<Long> timestampsAvailable = packetsAvailable.stream()
-				.map(TixPacketSerDe::getReportTimestamps)
-				.flatMap(List<Long>::stream)
-				.collect(Collectors.toList());
-		if (timestampsAvailable.size() < MIN_REQUIRED_REPORTS) {
-			logger.info("Installation {} delayed with {} measures", getInstallationId(), timestampsAvailable);
-			return Optional.empty();
-		}
-		
-		final List<TixDataPacket> toSubmit = packetsAvailable;
-		final List<Long> toDelete = toSubmit.stream()
-				.limit(toSubmit.size() / 2)
-				.map(TixPacketSerDe::getFirstReportTimestamp)
-				.collect(Collectors.toList());
-
-		logger.info("Installation {} extracts", getInstallationId());
-		return Optional.of(new SubmittablePacketSet() {
-			@Override
-			public List<TixDataPacket> packetsToSubmit() {
-				return toSubmit;
+				toSubmit = processable_reports;
+				toDelete = sampleStartTimes().subList(0, toSubmit.size() / 2);
 			}
 
-			@Override
-			public Optional<SubmittablePacketSet> onSubmitSuccess() throws IOException {
-				logger.info("Installation {} did submit", getId());
-				delete(toDelete);
-				return checkAndExtract();
-			}
+			logger.info("Installation {} builds submittable packet set", getInstallationId());
+			return Optional.of(new SubmittablePacketSet() {
+				@Override
+				public List<TixDataPacket> packetsToSubmit() {
+					return toSubmit;
+				}
 
-			@Override
-			public String getId() {
-				return Long.toString(getInstallationId());
-			}
-		});
+				@Override
+				public Optional<SubmittablePacketSet> onSubmitSuccess() throws IOException {
+					logger.info("Installation {} did submit", getId());
+					delete(toDelete);
+					return checkAndExtract();
+				}
+
+				@Override
+				public String getId() {
+					return Long.toString(getInstallationId());
+				}
+			});
+		}
+		return Optional.empty();
 	}
 }
